0421ee2e6d29da339bfa66ce898f505a1ef7966a
LuccasFontan <Lucasfontan2003@gmail.com>
2025-12-03 06:02:11 -0300

    commit

---
+++ JavaScript/Basico/Aula01.js
@@ -0,0 +1,6 @@
+// Console.log() serve para printarmos algo no terminal.
+console.log('Meu nome é "Lucas". Estou aprendendo JavaScript às 05:30 da manhã');
+
+/*
+    Serve para comentar em bloco
+*/
---
+++ JavaScript/Basico/Aula03.js
@@ -0,0 +1,10 @@
+// Não podemos criar variáveis com palavras reservadas Ex.: If, Else
+// Variáveos precisam ter nome significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+aaaa
+let nome = "Joao";
+console.log(nome)
